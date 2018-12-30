0c2d6a54f16eab4109a1e8137e01f3934c158ea9
Neev Parikh <neev_parikh@brown.edu>
2018-12-29 21:21:26 -0500

    Moved to antergos, created update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CFG='/home/neev/.config'
+
+# make dir if not exists
+mkdir -p i3 i3status termite rofi wallpaper
+
+# i3
+cp $CFG/i3/config $PWD/i3/i3-config
+cp $CFG/i3status/config $PWD/i3status/i3status-config
+
+# rofi
+cp $CFG/rofi/config $PWD/rofi/rofi-config
+cp /usr/share/rofi/themes/solarized.rasi $PWD/rofi/solarized.rasi
+
+# termite
+cp $CFG/termite/config $PWD/termite/termite-config
+
+# wallpapers
+cp /usr/share/backgrounds/*.png $PWD/wallpaper/
+
