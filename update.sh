ff8389e3db0d403a4a5d28580d332f3c9f0de613
Neev Parikh <neev_parikh@brown.edu>
2019-02-20 14:13:56 -0500

    Changed themes to gruvbox

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ CFG='/home/neev/.config'
 
 # make dir if not exists
 mkdir -p i3 i3status termite rofi wallpaper
-
+mkdir -p rofi/themes/
 # i3
 cp $CFG/i3/config $PWD/i3/i3-config
 cp $CFG/i3status/config $PWD/i3status/i3status-config
 
 # rofi
 cp $CFG/rofi/config $PWD/rofi/rofi-config
-cp /usr/share/rofi/themes/solarized.rasi $PWD/rofi/solarized.rasi
+cp /usr/share/rofi/themes/*.rasi $PWD/rofi/themes/
 
 # termite
 cp $CFG/termite/config $PWD/termite/termite-config
