2145ef27c52c35373d8abc13cfe934e8e77f1895
Neev Parikh <neev@stripe.com>
2023-12-11 13:07:01 -0800

    fix: remove broken scala lsp

--- config/nvim/ftplugin/scala.lua
+++
@@ -1,8 +0,0 @@
-local metals = require("metals")
-local metals_config = metals.bare_config()
-metals_config.settings = {
-  showImplicitArguments = true,
-}
-metals_config.init_options.statusBarProvider = "on"
-metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-metals.initialize_or_attach(metals_config)
