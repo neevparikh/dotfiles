05a4d5c298dbac578da593cffba476da862b52f4
Neev Parikh <neev@stripe.com>
2023-12-11 13:09:22 -0800

    fix: minor fixes for yabai helpers

--- config/skhd/helpers.sh
+++ config/skhd/helpers.sh
@@ -3,10 +3,46 @@
 get_layout_type() {
   out=$(yabai -m query --spaces --window mouse)
   length=$(echo $out | jq length)
-  if [ -z "$length"] || [$length -ne 1 ]; then 
+  if [ -z "$length" ] || [ $length -ne 1 ]; then 
     return -1
   fi
   echo $(echo $out | jq --raw-output ".[0].type")
+}
+
+get_opposite_dir() {
+  direction=$1
+  case $direction in
+  north)
+    echo "south";;
+  south)
+    echo "north";;
+  east)
+    echo "west";;
+  west)
+    echo "east";;
+  *)
+    exit -1;;
+  esac
+}
+
+yabai_move_to_desktop() {
+  direction=$1
+  opposite=$(get_opposite_dir $direction)
+
+  if ! yabai -m query --displays --display $direction ; then
+    return 1
+  fi
+
+  src=$(yabai -m query --windows --window | jq '.id')
+  dst=$(yabai -m query --windows --display $direction | jq 'map(select(."is-visible" == true))' | jq '.id?')
+  if [ -z "$dst" ]; then 
+    yabai -m window $src --display $direction
+  else 
+    yabai -m window $dst --insert $opposite                                              \
+        && yabai -m window --focus $src                                                  \
+        && yabai -m window --warp $dst                                                   
+  fi
+  yabai -m window --focus $src                                                  
 }
 
 yabai_move() {
@@ -22,16 +58,7 @@ yabai_move() {
       :;;
     esac
   elif [ "$layout" = "bsp" ]; then
-    case $direction in
-    north)
-      yabai -m window --warp $direction || yabai -m space --rotate 270;;
-    south)
-      yabai -m window --warp $direction || yabai -m space --rotate 90;;
-    east | west)
-      yabai -m window --warp $direction || yabai -m window --toggle split && yabai -m space --balance y-axis;;
-    *)
-      :;;
-    esac
+      yabai -m window --warp $direction || yabai_move_to_desktop $direction
   else
     return -1
   fi
