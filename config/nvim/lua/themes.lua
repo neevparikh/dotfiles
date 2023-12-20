7a189b8ca0a53fce5f0551777922b6d48cd5759f
Neev Parikh <neev@stripe.com>
2023-12-19 18:34:30 -0800

    fix: removed setting rooter elsewhere and moved fzf to theme

--- config/nvim/lua/themes.lua
+++ config/nvim/lua/themes.lua
@@ -25,4 +25,25 @@ vim.cmd("colorscheme gruvbox")
 vim.g.gruvbox_contrast_light = "medium"
 vim.g.gruvbox_contrast_dark = "hard"
 vim.opt.background = CheckTheme()
-vim.api.nvim_set_hl(0, "FloatBorder", { link = "GruvboxBg2" })
+
+vim.cmd([[
+" default
+let g:fzf_colors = {
+      \ 'fg':      ['fg', 'GruvboxFg1'],
+      \ 'bg':      ['fg', 'GruvboxBg0'],
+      \ 'hl':      ['fg', 'GruvboxYellow'],
+      \ 'fg+':     ['fg', 'GruvboxFg1'],
+      \ 'bg+':     ['fg', 'GruvboxBg1'],
+      \ 'hl+':     ['fg', 'GruvboxYellow'],
+      \ 'info':    ['fg', 'GruvboxBlue'],
+      \ 'prompt':  ['fg', 'GruvboxFg4'],
+      \ 'pointer': ['fg', 'GruvboxBlue'],
+      \ 'marker':  ['fg', 'GruvboxOrange'],
+      \ 'spinner': ['fg', 'GruvboxYellow'],
+      \ 'header':  ['fg', 'GruvboxBg3']
+      \ }
+
+let g:fzf_colors["gutter"] = ['bg', 'GruvboxBg1', 'Normal']
+let g:fzf_colors["bg+"] = ['bg', 'GruvboxBg1', 'Normal']
+let g:fzf_colors["info"] = ['bg', 'GruvboxBg2', 'Normal']
+]])
