2152d377b0daec99c7b54116334f18b4eba1f344
Neev Parikh <>
2023-12-27 18:33:35 -0500

    feat: added gruvbox colors properly

---
+++ config/sketchybar/gruvbox_light.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source gruvbox_common.sh
+
+export bg0_hard=0xfff9f5d7
+export bg0=0xfffbf1c7
+export bg0_soft=0xfff2e5bc
+export bg1=0xffebdbb2
+export bg2=0xffd5c4a1
+export bg3=0xffbdae93
+export bg4=0xffa89984
+export fg0_hard=0xff1d2021
+export fg0=0xff282828
+export fg0_soft=0xff32302f
+export fg1=0xff3c3836
+export fg2=0xff504945
+export fg3=0xff665c54
+export fg4=0xff7c6f64
+export specific_red_hard=0xfffc9690
+export specific_red=0xfffc9487
+export specific_red_soft=0xfff78b7f
+export specific_green_hard=0xffd3d6a5
+export specific_green=0xffd5d39b
+export specific_green_soft=0xffcecb94
+export specific_aqua_hard=0xffe6e9c1
+export specific_aqua=0xffe8e5b5
+export specific_aqua_soft=0xffe1dbac
