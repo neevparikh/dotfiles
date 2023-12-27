d9b98a67f780b6870a5544ad26e1d8a58605193c
Neev Parikh <>
2023-12-27 18:37:32 -0500

    feat: add vimdoc parser to fix error

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -203,30 +203,18 @@ require("nvim-treesitter.configs").setup({
     "gitcommit",
     "diff",
     "git_rebase",
+    "vimdoc",
   },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
   auto_install = false,
-
-  -- List of parsers to ignore installing (for "all")
   ignore_install = {},
 
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
-
-    -- list of language that will be disabled
     disable = {},
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
   matchup = {
     enable = true,
   },
